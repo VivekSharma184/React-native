6f3677b3afa53685f68cf7aca095efd49ad4b2fc
vivek sharma <vivicosharma@gmail.com>
2020-10-07 18:33:36 +0530

    RN component reuse and adding images

--- src/components/ImageDetails.js
+++ src/components/ImageDetails.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetails = (props) => {
   console.log(props);
   return (
     <View>
+      <Image source={require("../../assets/beach.jpg")} />
       <Text>{props.title}</Text>
     </View>
   );
