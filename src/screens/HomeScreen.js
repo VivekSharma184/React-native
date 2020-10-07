3e9d9b3355a4bcfb3709fee5667fb077d01c2dd5
vivek sharma <vivicosharma@gmail.com>
2020-10-07 16:39:55 +0530

    RN destructing Props

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
-  //console.log(props.navigation);
-
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen By VV</Text>
       <Button
-        onPress={() => props.navigation.navigate("Components")}
+        onPress={() => navigation.navigate("Components")}
         title="Go to Component demo"
       />
-      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
-        <Text>Go to List</Text>
-      </TouchableOpacity>
+
+      <Button
+        onPress={() => navigation.navigate("List")}
+        title="Go to List Demo"
+      />
     </View>
   );
 };
