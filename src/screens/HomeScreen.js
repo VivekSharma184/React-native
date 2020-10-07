a31b7053dfb98baefe9bf215136b50d3ffe12201
vivek sharma <vivicosharma@gmail.com>
2020-10-07 16:31:06 +0530

    RN Navigation components using buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
+  //console.log(props.navigation);
+
   return (
     <View>
       <Text style={styles.text}>HomeScreen By VV</Text>
       <Button
-        onPress={() => console.log(" Button Pressed  ")}
+        onPress={() => props.navigation.navigate("Components")}
         title="Go to Component demo"
       />
-      <TouchableOpacity onPress={() => console.log("List button pressed ")}>
-        <Text>Go to List</Text>
-        <Text>Go to List</Text>
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
         <Text>Go to List</Text>
       </TouchableOpacity>
     </View>
