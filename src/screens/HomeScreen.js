4c417ba160624133f053c7408319c6b60b37c40a
vivek sharma <vivicosharma@gmail.com>
2020-10-07 23:17:12 +0530

    RN State Management In Components

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -19,6 +19,11 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate("Image")}
         title="Go to Image Demo"
       />
+
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to Counter Demo "
+      />
     </View>
   );
 };
