517afe3cf5b8e9e1277be16795aa0d4ce20ed11a
vivek sharma <vivicosharma@gmail.com>
2020-10-09 11:15:46 +0530

    RN Before validation of color

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,28 +8,30 @@ const [red , setRed] = useState(0);
 const [green , setGreen ] = useState(0);
 const [blue , setBlue ] = useState(0);
 
-console.log(red);
+const COLOR_INCREAMENT = 15;
 
 return <View>
     <ColorCounter 
-    onIncrease = {()=> setRed(red + 1 )}
-    onDecrease = {() => setRed(red - 1 )}
+    onIncrease = {()=> setRed(red + COLOR_INCREAMENT )}
+    onDecrease = {() => setRed(red - COLOR_INCREAMENT )}
     color='RED'
     />
     <ColorCounter 
-    onIncrease = {()  => setGreen(green + 1)}
-    onDecrease = {()=> setGreen(green -1 )}
+    onIncrease = {()  => setGreen(green + COLOR_INCREAMENT)}
+    onDecrease = {()=> setGreen(green - COLOR_INCREAMENT )}
     color='Green' 
     />
     <ColorCounter 
-    onIncrease = {() => setBlue(blue + 1)}
-    onDecrease = {()=> setBlue(blue -1 )}
+    onIncrease = {() => setBlue(blue + COLOR_INCREAMENT)}
+    onDecrease = {()=> setBlue(blue - COLOR_INCREAMENT )}
     color ='Blue' 
     />
+
+    <View style={{width:100 , height:100,  backgroundColor:`rgb(${red} , ${green} , ${blue}  )` }} />
 </View>
 
 }
-
+ 
 const styles = StyleSheet.create({});
 
 export default SquareScreen ;
